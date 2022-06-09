e91e9c6f465b1675b777fb5966e6f2d128e7e61f
Emanuele Colabello <emanuele.colabello@gmail.com>
2022-06-09 10:03:20 +0200

    first commit

---
+++ src/Container/AppFunction.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import NewTask from "../Presentational/NewTask";
+import TasksList from "../Presentational/TasksList";
+
+export default function AppFunction() {
+  // hook your code up here ;)
+  const [newTask, setNewTask] = useState({});
+  const [allTasks, setAllTasks] = useState([]);
+
+  const handleChange = ({target}) => {
+    const { name, value } = target;
+    setNewTask(prevTask => ({
+      ...prevTask,
+      [name] : value,
+      id : Date.now()
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(!newTask.title) return;
+
+    setAllTasks(prevAllTasks => [
+      newTask,
+      ...prevAllTasks
+    ]);
+  };
+  const handleDelete = (taskIdToRemove) => {
+
+    setAllTasks(prevAllTasks => prevAllTasks.filter(task => task.id !== taskIdToRemove));
+  };
+
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+    </main>
+  );
+}
